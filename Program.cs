a32c9f3bbf5d5f86dfa4385fb9f11287fec51365
alaminsheikh01 <alaminsheikh9746@gmail.com>
2023-09-18 17:08:14 +0600

    added different way to handle the data

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using linq_practice;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,33 +12,38 @@ namespace LinqSample
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>() {1,2,3,4,5,6 };
-
             List<Employee> employees = new List<Employee>()
             {
-                new Employee(){Id=1, Name="Tom"},
-                new Employee(){Id=2, Name="John" }
+              new Employee() { Id = 1, Name = "Tom", Email = "tom@gmail.com" },
+              new Employee() { Id = 2, Name = "Tom1", Email = "tom1@gmail.com" },
+              new Employee() { Id = 3, Name = "Tom2", Email = "tom2@gmail.com" },
+              new Employee() { Id = 4, Name = "Tom3", Email = "tom3@gmail.com" }
 
             };
 
-            //IEnumerable<Employee> query = from emp in employees
-            //                              where emp.Id == 2
-            //                              select emp;
+            var selectQuery = (from emp in employees
+                               select new Employee()
+                               {
+                                   Id = emp.Id,
+                                   Name = emp.Name,
+                                   Email = emp.Email,
+                               }).ToList();
 
-            IQueryable<Employee> query1 = employees.AsQueryable().Where(x => x.Id == 1);
-
-            foreach(var item in query1)
+            var selectMethod = employees.Select(emp => new Employee()
             {
-                Console.WriteLine("ID = "+ item.Id + " And Name = " + item.Name);
+                Id = emp.Id,
+                Name = emp.Name,
+                Email = emp.Email
+            }).ToList();
+
+            var query = employees.Select((emp, index) => new { index = index, Name = emp.Name }).ToList();
+
+            foreach(var item in query)
+            {
+                Console.WriteLine("Name " + item.Name);
             }
 
-            Console.ReadLine();
         }
-        class Employee
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-        }
-    
+
     }
 } 
